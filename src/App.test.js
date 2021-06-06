cb42e37725f9a2a30ff3c7885e7d8fc36c6d6653
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2021-06-06 10:29:41 +0530

    Updated test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
